4e824fd5b6b5cab71d823a152045dce11543eee4
Naman Sharma <namansharma50962@gmail.com>
2024-06-18 00:47:18 +0530

    initial commit

---
+++ color-flipper/index.js
@@ -0,0 +1,21 @@
+
+const toGreen = () => {
+    window.console.log('change color to green');
+    window.document.body.style.backgroundColor = 'green';
+}
+
+const toRed = () => {
+    window.console.log('change color to red');
+    window.document.body.style.backgroundColor = 'red';
+}
+
+const toBlue = () => {
+    window.console.log('change color to blue');
+    window.document.body.style.backgroundColor = 'blue';
+}
+
+const toRandom = () => {
+
+    window.console.log('changed color');
+    window.document.body.style.backgroundColor = 'yellow';
+}
