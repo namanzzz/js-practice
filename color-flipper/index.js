d8317eac80ee063bf023456b03dea80a970873d7
Naman Sharma <namansharma50962@gmail.com>
2024-06-18 01:42:33 +0530

    final commit

--- color-flipper/index.js
+++ color-flipper/index.js
@@ -1,21 +1,36 @@
 
-const toGreen = () => {
-    window.console.log('change color to green');
-    window.document.body.style.backgroundColor = 'green';
-}
+const heading = document.getElementsByClassName('heading')[0];
 
-const toRed = () => {
-    window.console.log('change color to red');
-    window.document.body.style.backgroundColor = 'red';
-}
+// const toGreen = () => {
+//     window.console.log('change color to green');
+//     window.document.body.style.backgroundColor = 'green';
+//     heading.textContent = 'Color Green'
+// }
 
-const toBlue = () => {
+// const toRed = () => {
+//     window.console.log('change color to red');
+//     window.document.body.style.backgroundColor = 'red';
+//     heading.textContent = 'Color Red'
+// }
+
+// const toBlue = () => {
+//     window.console.log('change color to blue');
+//     window.document.body.style.backgroundColor = 'blue';
+//     heading.textContent = 'Color Blue'
+// }
+
+// repeated code above, need to optimize
+const toColor = (color) => {
     window.console.log('change color to blue');
-    window.document.body.style.backgroundColor = 'blue';
+    window.document.body.style.backgroundColor = `${color}`;
+    heading.textContent = `Changed to ${color}`;
 }
 
 const toRandom = () => {
-
-    window.console.log('changed color');
-    window.document.body.style.backgroundColor = 'yellow';
+    // RGB 
+    const red = Math.round(Math.random()*255);
+    const green = Math.round(Math.random()*255);
+    const blue = Math.round(Math.random()*255);
+    window.document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    heading.textContent = `Changed to rgb(${red},${green},${blue})`;
 }
